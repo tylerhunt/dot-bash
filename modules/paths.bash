9f881c286bc3e11a47c2e18aba6bf58b9805f007
Tyler Hunt <tyler@tylerhunt.com>
2013-08-06 15:09:03 -0700

    Add Heroku Toolbelt path to $PATH.

--- modules/paths.bash
+++ modules/paths.bash
@@ -2,6 +2,10 @@ load-paths() {
   export PATH="/usr/local/bin:${PATH}"
   export PATH="/usr/local/sbin:${PATH}"
 
+  if [ -d /usr/local/heroku/bin ]; then
+    export PATH="/usr/local/heroku/bin:${PATH}"
+  fi
+
   if [ -d ~/.bin ]; then
     export PATH="~/.bin:${PATH}"
   fi
