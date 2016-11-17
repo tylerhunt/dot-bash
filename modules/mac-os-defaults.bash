9aa0d203cda2cea751e4bc114773c84fde65f8b3
Tyler Hunt <tyler@tylerhunt.com>
2016-11-16 22:34:45 -0500

    Disable older Mac OS defaults.

--- modules/mac-os-defaults.bash
+++ modules/mac-os-defaults.bash
@@ -3,23 +3,23 @@ load-mac-os-defaults() {
     [ -f ~/.bash/.run-once ] && return
 
     # Finder
-    defaults write com.apple.finder EmptyTrashSecurely -bool true
-    defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-    defaults write com.apple.finder QLEnableTextSelection -bool true
+    #defaults write com.apple.finder EmptyTrashSecurely -bool true
+    #defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+    #defaults write com.apple.finder QLEnableTextSelection -bool true
     defaults write com.apple.finder ShowStatusBar -bool true
     #defaults write com.apple.finder CreateDesktop -bool false
     killall Finder
 
     # Dock
     defaults write com.apple.dock autohide -bool true
-    defaults write com.apple.dock no-glass -boolean true
-    defaults write com.apple.dock use-new-list-stack -bool true
+    #defaults write com.apple.dock no-glass -boolean true
+    #defaults write com.apple.dock use-new-list-stack -bool true
     killall Dock
 
     # Safari
     defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
     defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
-    defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+    #defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 
     # Messages
     defaults write com.apple.iChat SaveConversationsOnClose -bool true
