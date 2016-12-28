0a34e196b8f161b2af5de9978d027df7a247cb5e
Tyler Hunt <tyler@tylerhunt.com>
2016-12-28 15:45:43 -0500

    Add GPG module.

---
+++ modules/gpg.bash
@@ -0,0 +1,16 @@
+# Based on https://gist.github.com/bmhatfield/cc21ec0a3a2df963bffa3c1f884b676b.
+
+load-gpg() {
+  # Load gpg-agent, assuming it was installed.
+  if is-executable gpg-agent ; then
+    if [ -f ~/.gnupg/.gpg-agent-info ] && [ -n "$(pgrep gpg-agent)" ]; then
+      source ~/.gnupg/.gpg-agent-info
+      export GPG_AGENT_INFO
+    else
+      eval $(gpg-agent --daemon --write-env-file ~/.gnupg/.gpg-agent-info)
+    fi
+  fi
+
+  # Define TTY for interactive GPG commands.
+  export GPG_TTY=$(tty)
+}
