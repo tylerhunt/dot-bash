549c2a39a96ea89eb01be879aca15a63034f5bfc
Tyler Hunt <tyler@tylerhunt.com>
2018-08-22 16:47:38 -0400

    Add function for easily auto-squashing git fixups.

--- modules/git.bash
+++ modules/git.bash
@@ -5,4 +5,10 @@ load-git() {
   alias st="git status"
   alias cm="git commit"
   alias wip="git commit -m WIP"
+
+  # Functions
+  fix() {
+    if [ $# -eq 0 ]; then echo "Usage: fix [ref]" && return; fi
+    git rebase -i --autosquash "$1^"
+  }
 }
