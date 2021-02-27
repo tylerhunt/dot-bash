624841c7447dbd993514f3e6a293d77efb07e829
Tyler Hunt <tyler@tylerhunt.com>
2021-02-26 23:30:04 -0500

    Enable SSL support in PostgreSQL

--- modules/asdf.bash
+++ modules/asdf.bash
@@ -1,6 +1,8 @@
 load-asdf() {
   [ ! -d ~/.asdf ] && return 1
 
+  export POSTGRES_EXTRA_CONFIGURE_OPTIONS="--with-uuid=e2fs --with-openssl --with-libraries=/usr/local/lib:/usr/local/opt/openssl@1.1/lib --with-includes=/usr/local/include:/usr/local/opt/openssl@1.1/include"
+
   . $HOME/.asdf/asdf.sh
   . $HOME/.asdf/completions/asdf.bash
 }
