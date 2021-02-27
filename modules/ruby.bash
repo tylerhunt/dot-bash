525018606dd917d35143e4ec106460e9ed684576
Tyler Hunt <tyler@tylerhunt.com>
2021-02-26 23:31:55 -0500

    Remove old Ruby configuration

--- modules/ruby.bash
+++ modules/ruby.bash
@@ -1,12 +1,4 @@
 load-ruby() {
-  # Load rbenv, assuming is was installed via Homebrew.
-  if is-executable rbenv ; then
-    eval "$(rbenv init -)"
-  fi
-
-  # List of commands that shouldn't use rubygems-bundler.
-  export NOEXEC_EXCLUDE="powder"
-
   # Aliases
   alias frspec="rspec --only-failures"
   alias ffrspec="rspec --next-failure"
