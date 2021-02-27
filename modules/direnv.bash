d3e7e5921ba45eb20d17fd4c005c78974535fa5c
Tyler Hunt <tyler@tylerhunt.com>
2021-02-26 23:29:32 -0500

    Install and configure asdf-direnv

--- modules/direnv.bash
+++ modules/direnv.bash
@@ -1,5 +1,15 @@
 load-direnv() {
   is-executable direnv || return 1
 
+  if is-executable asdf ; then
+    # NOTE: add 'use asdf' to .envrc files to utilize asdf-direnv
+    if [ -z "$(asdf plugin list | grep direnv)" ]; then
+      asdf plugin-add direnv
+      asdf install direnv
+      asdf global direnv system
+      mkdir -p ~/.asdf/installs/direnv/system/env
+    fi
+  fi
+
   eval "$(direnv hook bash)"
 }
