dbae21fcd461e87b6aeede8828799d63a92dd946
Tyler Hunt <tyler@tylerhunt.com>
2021-03-25 19:17:50 -0400

    Update Homebrew autocomplete path

--- modules/homebrew.bash
+++ modules/homebrew.bash
@@ -1,7 +1,7 @@
 load-homebrew() {
   is-executable brew || return 1
 
-  local brew_completion="$(brew --prefix)/etc/bash_completion"
+  local brew_completion="$(brew --prefix)/etc/profile.d/bash_completion.sh"
 
   [ -f $brew_completion ] && source $brew_completion
 }
