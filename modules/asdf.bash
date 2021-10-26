ae58a2c0cdc7f73525ced6116ee159910a82d105
Tyler Hunt <tyler@tylerhunt.com>
2021-10-26 09:59:21 -0400

    Set Erlang build flags

--- modules/asdf.bash
+++ modules/asdf.bash
@@ -1,6 +1,7 @@
 load-asdf() {
   [ ! -d ~/.asdf ] && return 1
 
+  export KERL_CONFIGURE_OPTIONS="--disable-debug --disable-silent-rules --without-javac --enable-shared-zlib --enable-dynamic-ssl-lib --enable-hipe --enable-smp-support --enable-threads --enable-kernel-poll --enable-wx --enable-darwin-64bit --with-ssl=$(brew --prefix openssl)"
   export POSTGRES_EXTRA_CONFIGURE_OPTIONS="--with-uuid=e2fs --with-openssl --with-libraries=/usr/local/lib:/usr/local/opt/openssl@1.1/lib --with-includes=/usr/local/include:/usr/local/opt/openssl@1.1/include"
 
   . $(brew --prefix)/opt/asdf/libexec/asdf.sh
