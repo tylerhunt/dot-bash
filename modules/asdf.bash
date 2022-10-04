f234c014974a4e72833169e65e562bbde85e786a
Tyler Hunt <tyler@tylerhunt.com>
2022-10-04 08:02:42 -0400

    Use Homebrew prefix to load asdf

--- modules/asdf.bash
+++ modules/asdf.bash
@@ -3,6 +3,6 @@ load-asdf() {
 
   export POSTGRES_EXTRA_CONFIGURE_OPTIONS="--with-uuid=e2fs --with-openssl --with-libraries=/usr/local/lib:/usr/local/opt/openssl@1.1/lib --with-includes=/usr/local/include:/usr/local/opt/openssl@1.1/include"
 
-  . $HOME/.asdf/asdf.sh
-  . $HOME/.asdf/completions/asdf.bash
+  . $(brew --prefix)/opt/asdf/libexec/asdf.sh
+  . $(brew --prefix)/etc/bash_completion.d/asdf.bash
 }
