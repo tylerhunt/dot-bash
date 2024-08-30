88fd52290d008934c3c63448e6be36bbb23afa4d
Tyler Hunt <tyler@tylerhunt.com>
2024-08-30 19:25:10 -0400

    Use `python3` and ensure it’s available

--- modules/ssh.bash
+++ modules/ssh.bash
@@ -1,9 +1,11 @@
 load-ssh() {
+  is-executable python3 || return 1
+
   if [ ! -d ~/.bin/ssh-ident ]; then
     git clone https://github.com/ccontavalli/ssh-ident.git ~/.bin/ssh-ident
   fi
 
-  alias ssh='asdf exec python ~/.bin/ssh-ident/ssh-ident'
-  alias scp='BINARY_SSH=scp asdf exec python ~/.bin/ssh-ident/ssh-ident'
-  alias sfpt='BINARY_SSH=sftp asdf exec python ~/.bin/ssh-ident/ssh-ident'
+  alias ssh='asdf exec python3 ~/.bin/ssh-ident/ssh-ident'
+  alias scp='BINARY_SSH=scp asdf exec python3 ~/.bin/ssh-ident/ssh-ident'
+  alias sfpt='BINARY_SSH=sftp asdf exec python3 ~/.bin/ssh-ident/ssh-ident'
 }
