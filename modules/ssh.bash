603ba38da613b0c5091077b997daa9848f8c7d83
Tyler Hunt <tyler@tylerhunt.com>
2025-01-12 15:07:38 -0500

    Install and use local Python for `ssh-ident`

--- modules/ssh.bash
+++ modules/ssh.bash
@@ -1,11 +1,23 @@
 load-ssh() {
   is-executable python3 || return 1
 
+  local ssh_ident_dir="${HOME}/.bin/ssh-ident"
+
   if [ ! -d ~/.bin/ssh-ident ]; then
-    git clone https://github.com/ccontavalli/ssh-ident.git ~/.bin/ssh-ident
+    git clone https://github.com/ccontavalli/ssh-ident.git "${ssh_ident_dir}"
+    pushd "${ssh_ident_dir}"
+    echo 'python 3.12.5' > .tool-versions
+    asdf install
+    popd
   fi
 
-  alias ssh='asdf exec python3 ~/.bin/ssh-ident/ssh-ident'
-  alias scp='BINARY_SSH=scp asdf exec python3 ~/.bin/ssh-ident/ssh-ident'
-  alias sfpt='BINARY_SSH=sftp asdf exec python3 ~/.bin/ssh-ident/ssh-ident'
+  eval "ssh_ident() {
+    bash \
+      --rcfile <(echo \"cd ${ssh_ident_dir}\") \
+      -ci \"${ssh_ident_dir}/ssh-ident \$@\"
+  }"
+
+  alias ssh='ssh_ident'
+  alias scp='BINARY_SSH=scp ssh_ident'
+  alias sftp='BINARY_SSH=sftp ssh_ident'
 }
