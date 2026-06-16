306a4e8565e3f82cf8f23aad51c02cfbc3fe1502
Tyler Hunt <tyler@tylerhunt.com>
2026-06-16 16:44:32 -0400

    Remove `asdf` plugin for `direnv`

--- modules/direnv.bash
+++ modules/direnv.bash
@@ -1,14 +1,6 @@
 load-direnv() {
   is-executable direnv || return 1
 
-  if is-executable asdf ; then
-    # NOTE: add 'use asdf' to .envrc files to utilize asdf-direnv
-    if [ -z "$(asdf plugin list | grep direnv)" ]; then
-      asdf plugin-add direnv
-      asdf global direnv system
-    fi
-  fi
-
   eval "$(direnv hook bash)"
 
   # load asdf-direnv
