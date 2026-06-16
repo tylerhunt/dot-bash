b8e94729e495ebaef3eda2cebdedbbcb1f274de9
Tyler Hunt <tyler@tylerhunt.com>
2026-06-16 16:47:19 -0400

    Add `branch_down` function to reverse migrations

--- modules/ruby.bash
+++ modules/ruby.bash
@@ -10,4 +10,11 @@ load-ruby() {
     if [ $# -eq 0 ]; then echo "Usage: agrspec [term]" && return; fi
     rspec -fd `ag -l -G '_spec.rb$' $1 spec`
   }
+  branch_down() {
+    local branch=${1:-main}
+
+    for version in `git diff --name-only --diff-filter=A ${branch}.. db/migrate/ | sed -r -n 's/^.*\/([^_]+).*$/\1/p' | tac` ; do
+      VERSION=$version rails db:migrate:down
+    done
+  }
 }
